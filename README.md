7bea4834bd0949c9c770a762b6fb57773b425550
Alex Robson <asrobson@gmail.com>
2012-09-16 19:20:12 -0500

    Updates to output plugin to add copy feature

--- README.md
+++ README.md
@@ -5,3 +5,119 @@ This plugin is a core component of anvil and is required to function as expected
 ## Installation
 
 anvil will install this plugin during post-install.
+
+## Normal Output
+
+This plugin will read the output path or list of output paths. For each path in the output path, all output will be copied relative to their source path structure.
+
+### Example Source Structure
+
+/src
+  | a.js
+  | b.css
+  | c.html
+  | - subdir
+    | d.js
+    | e.css
+    | f.html
+
+### Build File 1
+
+'''
+{
+	"output": "lib"
+}
+'''
+
+**output structure**
+
+/lib
+  | a.js
+  | b.css
+  | c.html
+  | - subdir
+    | d.js
+    | e.css
+    | f.html
+
+### Build File 2
+
+'''
+{
+	"output": [ "lib", "site" ]
+}
+'''
+
+**output structure**
+
+/lib
+  | a.js
+  | b.css
+  | c.html
+  | - subdir
+    | d.js
+    | e.css
+    | f.html
+
+/output
+  | a.js
+  | b.css
+  | c.html
+  | - subdir
+    | d.js
+    | e.css
+    | f.html
+
+## Additional Copy Control
+You can further control what build files get copied to where using the "anvil.output": { "copy": {} } build option. The copy property is a hash where the key is a minimatch (glob) format of the files to be copied and the value is a path or paths to copy all matched files to.
+
+### Example Source Structure
+
+/src
+  | a.js
+  | b.css
+  | c.html
+  | - subdir
+    | d.js
+    | e.css
+    | f.html
+
+### Build File 1
+'''
+{
+	"anvil.output": {
+		"copy": {
+			"**/*.js": "js"
+		}
+	}
+}
+'''
+
+***output structure***
+
+/js
+  | a.js
+  | b.js
+
+Note: the files output in the matter are output flat and to not retain the structure they were pulled from.
+
+### Build File 2
+'''
+{
+	"anvil.output": {
+		"copy": {
+			"**/*.css": "css",
+			"**/*.html": "html"
+		}
+	}
+}
+'''
+
+***output structure***
+
+/css
+  | b.css
+  | e.css
+/html
+  | c.html
+  | f.html
--- src/output.js
+++ src/output.js
@@ -1,21 +1,43 @@
 var path = require( "path" );
+var minimatch = require( "minimatch" );
 
 module.exports = function( _, anvil ) {
 	return anvil.plugin( {
 		name: "anvil.output",
 		activity: "push",
-
+		config: {
+			copy: {
+			}
+		},
 		clean: function( done ) {
-			anvil.fs.cleanDirectory( anvil.config.output, function( err ) {
-				if( err ) {
-					anvil.log.error( err );
-				}
-				done();
-			} );
+			var self = this,
+				list = this.getOutputList(),
+				base = path.resolve( "./" );
+			anvil.scheduler.parallel( list, function( spec, done ) {
+				anvil.scheduler.parallel( spec.directories, function( directory, done ) {
+					anvil.fs.cleanDirectory( directory, function( err ) {
+						if( err ) {
+							anvil.log.error( "Error cleaning " + directory + " : " + err.stack );
+						}
+						done();
+					} );
+				}, done );
+			}, done );
 		},
 
 		configure: function( config, command, done ) {
-			var self = this;
+			var self = this,
+				pluginConfig = anvil.config[ this.name ],
+				all = pluginConfig.copy[ "**/*" ],
+				outputList = _.isArray( anvil.config.output ) ? anvil.config.output : [ anvil.config.output ];
+				outputList = _.map( outputList, function( outputPath ) {
+					return path.join( outputPath, "{relative}" );
+				} );
+			if( all ) {
+				all = all.concat( outputList );
+			} else {
+				pluginConfig.copy[ "**/*" ] = outputList;
+			}
 			anvil.events.on( "file.deleted", function( change, path, base ) {
 				if( base === anvil.config.source ) {
 					self['delete']( path );
@@ -24,11 +46,19 @@ module.exports = function( _, anvil ) {
 			done();
 		},
 
-		copy: function( file, done ) {
-			anvil.scheduler.parallel( [ anvil.config.output ], function( destination, copied ) {
-				destination = path.resolve( destination );
-				anvil.log.debug( "copying " + file.name + " to " + ( destination + file.relativePath ) );
-				anvil.fs.copy( [ file.workingPath, file.name ], [ destination, file.relativePath, file.name ], copied );
+		copy: function( done ) {
+			var self = this,
+				list = this.getOutputList(),
+				base = path.resolve( "./" );
+			anvil.scheduler.parallel( list, function( spec, done ) {
+				var files = self.getFilesForPattern( spec.pattern );
+				anvil.scheduler.parallel( spec.directories, function( directory, done ) {
+					anvil.scheduler.parallel( files, function( file, written ) {
+						var relativePath = directory.replace( "{relative}", file.relativePath );
+						anvil.log.debug( "copying " + file.name + " to " + relativePath );
+						anvil.fs.copy( [ file.workingPath, file.name ], [ relativePath, file.name ], written );
+					}, done );
+				}, done );
 			}, done );
 		},
 
@@ -38,30 +68,32 @@ module.exports = function( _, anvil ) {
 						} );
 			if( file ) {
 				anvil.scheduler.parallel( [ anvil.config.output ], function( destination, done ) {
-					try {
-						destination = path.resolve( destination );
-						var removeFrom = anvil.fs.buildPath( [ destination, file.relativePath, file.name ] );
-						anvil.log.debug( "Deleting output at " + removeFrom );
-						anvil.fs["delete"]( removeFrom, done );
-					} catch( err ) {
-						console.log( err );
-					}
+					destination = path.resolve( destination );
+					var removeFrom = anvil.fs.buildPath( [ destination, file.relativePath, file.name ] );
+					anvil.log.debug( "Deleting output at " + removeFrom );
+					anvil.fs["delete"]( removeFrom, done );
 				}, function() {} );
 			}
 		},
 
-		run: function( done ) {
-			var self = this,
-				toCopy = _.filter( anvil.project.files, function( file ) {
-				return !file.noCopy;
+		getFilesForPattern: function( pattern ) {
+			return _.filter( anvil.project.files, function( file ) {
+				var relativePath = anvil.fs.buildPath( [ file.relativePath, file.name ] );
+				return !file.noCopy && minimatch.match( [ relativePath ], pattern, {} ).length > 0;
 			} );
+		},
+
+		getOutputList: function() {
+			return _.map( anvil.config[ this.name ].copy, function( directories, pattern ) {
+				directories = _.isArray( directories ) ? directories : [ directories ];
+				return { pattern: pattern, directories: directories };
+			} );
+		},
+
+		run: function( done ) {
+			var self = this;
 			this.clean( function() {
-				anvil.scheduler.parallel( toCopy, self.copy, function() {
-					_.each( anvil.project.files, function( file ) {
-						file.state = "done";
-					} );
-					done();
-				} );
+				self.copy( done );
 			} );
 		}
 	} );
